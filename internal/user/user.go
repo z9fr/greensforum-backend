7431cbb442b530b825f9c1a62c3ab7e10284168a
z9fr <z9fr@protonmail.com>
2022-04-28 06:02:51 +0530

    user struct and interfaces

--- internal/user/user.go
+++ internal/user/user.go
@@ -1,22 +1,50 @@
 package user
 
+import "gorm.io/gorm"
+
+type Interests struct {
+	Tags []string `json:"tags"`
+}
+
 type User struct {
-	AccountID    int    `json:"account_id"`
-	IsEmployee   bool   `json:"is_employee"`
-	Reputation   int    `json:"reputation"`
-	UserType     string `json:"user_type"`
+	ID       uint64
+	Username string  `gorm:"column:username" json:"username"`
+	Email    string  `gorm:"column:email" json:"email"`
+	Password string  `gorm:"column:password" json:"password"`
+	UserType string  `json:"user_type"`
+	UserAcc  Account `gorm:"references:AccountID"`
+}
+
+type Account struct {
+	AccountID    uint64 `gorm:"primaryKey" json:"account_id"`
 	UserID       int    `json:"user_id"`
 	Location     string `json:"location"`
 	WebsiteURL   string `json:"website_url"`
 	Link         string `json:"link"`
 	ProfileImage string `json:"profile_image"`
 	DisplayName  string `json:"display_name"`
-	Role         string `json:"role"`
 	Description  string `json:"description"`
 	Name         string `json:"name"`
 	Slug         string `json:"slug"`
+	IsEmployee   bool   `json:"is_employee"`
+	Reputation   int    `json:"reputation"`
 }
 
-type Interests struct {
-	Tags []string `json:"tags"`
+type Service struct {
+	DB *gorm.DB
+}
+
+type UserService interface {
+	CreateUser(user User) (User, error)
+}
+
+func NewService(db *gorm.DB) *Service {
+	return &Service{
+		DB: db,
+	}
+}
+
+func (s *Service) CreateUser(user User) (User, error) {
+	// not implemented
+	return user, nil
 }
