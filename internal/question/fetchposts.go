a498bbcf3d30b958ace0acfe2aeedd9869429df3
z9fr <z9fr@protonmail.com>
2022-05-04 13:47:59 +0530

    refactor code

---
+++ internal/question/fetchposts.go
@@ -0,0 +1,25 @@
+package question
+
+// fetch all posts avaible
+func (s *Service) GetAllPosts() []Question {
+	var questions []Question
+	s.DB.Debug().Preload("Tags").Preload("Answers").Find(&questions)
+
+	return questions
+}
+
+// GetArticles returns all articles from the database
+func (s *Service) GetQuestionsPaginate(pageID int) PaginatedQuestions {
+	var response PaginatedQuestions
+	var questions []Question
+	s.DB.Where("id >= ? ", pageID).Order("id").Limit(pageSize + 1).Find(&questions)
+
+	if len(questions) == pageSize+1 {
+		next_id := questions[len(response.Items)-1].ID
+		uq := questions[:pageSize]
+		response.Items = append(response.Items, uq...)
+		response.Next = int(next_id)
+	}
+
+	return response
+}
---
+++ internal/question/vote.go
@@ -0,0 +1,8 @@
+package question
+
+import "github.com/z9fr/greensforum-backend/internal/user"
+
+// upvote a post
+func (s *Service) UpVotePost(user user.User, question Question) {
+
+}
